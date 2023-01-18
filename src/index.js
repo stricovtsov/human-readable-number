2bf5623c7064c3ff8f066c6199408f464f4e984d
stricovtsov <strik85@tut.by>
2023-01-18 21:23:25 +0300

    feat:add the solution of the problem

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function toReadable (number) {
-  
+   let b = number%10, a = (number-b)/10,
+        A1 = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'],
+        A2 = ['eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen','sixteen', 'seventeen', 'eighteen', 'nineteen'],
+        A3 = ['ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
+    if (number > 10 && number < 20) return A2[number-11];
+    if (b == 0) return A3[a-1];
+    return A3[a-1]+' '+A1[b-1];
 }
